9ca426a3ecee12042da0fcbd16eeacf3f383fcce
PedroMouraTI <48687882+phvm@users.noreply.github.com>
2021-04-13 04:46:38 -0300

    Resolvido problema no Dijkstra e adicionado função

--- Grafo.py
+++ Grafo.py
@@ -2,6 +2,13 @@ class Grafo:
     def __init__(self, grafo):
         self.grafo = grafo
 
+    def remover_vert(self, vertice):
+        for s in range(1, len(self.grafo)):
+            if vertice in self.grafo[s].keys():
+                del self.grafo[s][vertice]
+        del self.grafo[vertice]
+        return "Defeito informado."
+
     def remover_ares(self, vert1, vert2):
         try:
             del self.grafo[vert1][vert2]
@@ -13,35 +20,37 @@ class Grafo:
     def dijkstra(self, src, dest):
         men_dist = {}
         antecessor = {}
-        nao_visitado = self.grafo
+        nao_visitado = self.grafo.copy()
         infinito = float("inf")
         caminho = []
-
-        for node in nao_visitado:
-            men_dist[node] = infinito
-        men_dist[src] = 0
-        while nao_visitado:
-            minNode = None
+        try:
             for node in nao_visitado:
-                if minNode is None:
-                    minNode = node
-                elif men_dist[node] < men_dist[minNode]:
-                    minNode = node
-            for filho, peso in self.grafo[minNode].items():
-                if peso + men_dist[minNode] < men_dist[filho]:
-                    men_dist[filho] = peso + men_dist[minNode]
-                    antecessor[filho] = minNode
-            nao_visitado.pop(minNode)
-        atual = dest
-        while atual != src:
-            try:
-                caminho.insert(0, atual)
-                atual = antecessor[atual]
-            except KeyError:
-                return str(src) + " nao é conectavel com " + str(dest)
-        caminho.insert(0, src)
-        if men_dist[dest] != infinito:
-            vazia = ""
-            for l in range(len(caminho)):
-                vazia += str(caminho[l]) + " "
-            return vazia
+                men_dist[node] = infinito
+            men_dist[src] = 0
+            while nao_visitado:
+                minNode = None
+                for node in nao_visitado:
+                    if minNode is None:
+                        minNode = node
+                    elif men_dist[node] < men_dist[minNode]:
+                        minNode = node
+                for filho, peso in self.grafo[minNode].items():
+                    if peso + men_dist[minNode] < men_dist[filho]:
+                        men_dist[filho] = peso + men_dist[minNode]
+                        antecessor[filho] = minNode
+                nao_visitado.pop(minNode)
+            atual = dest
+            while atual != src:
+                try:
+                    caminho.insert(0, atual)
+                    atual = antecessor[atual]
+                except KeyError:
+                    return str(src) + " não é conectável com " + str(dest)
+            caminho.insert(0, src)
+            if men_dist[dest] != infinito:
+                vazia = ""
+                for l in range(len(caminho)):
+                    vazia += str(caminho[l]) + " "
+                return vazia
+        except KeyError:
+            pass
--- Main.py
+++ Main.py
@@ -7,7 +7,13 @@ data = insert(4941, path)
 grafo = Grafo(data)
 
 st.title("Reconectando")
-#st.write("")
+st.text("Nesse projeto, usamos a base de dados que descreve a malha de conexões elétricas do \noeste dos Estados Unidos"
+        ". Aqui você pode selecionar algumas opções, como buscar \na menor distância entre dois pontos,"
+        " informar uma falha em uma das conexões entre dois \npontos ou um defeito em um ponto.")
+st.text("Nesta aplicação, cada ponto representa um transformador, gerador ou uma subestação.\n"
+        "Ao ser informado alguma falha ou defeito, a aplicação é capaz de modificar a base de \ndados e informar"
+        " caso haja alguma outra conexão que possa ser feita para que uma \nregião não fique"
+        " sem energia elétrica.")
 
 options = list(range(1, 4942))
 st.subheader("Buscar menor caminho")
@@ -17,8 +23,13 @@ if st.button("Buscar"):
     caminho = grafo.dijkstra(int(src), int(dest))
     st.write(caminho)
 st.subheader("Informar falha em um conexão")
-dot1 = st.selectbox("Primeiro ponto", options, index=0)
-dot2 = st.selectbox("Segundo ponto", options, index=0)
-if st.button("Informar falha."):
-    msg = grafo.remover_ares(int(dot1), int(dot2))
+vert1 = st.selectbox("Primeiro ponto", options, index=0)
+vert2 = st.selectbox("Segundo ponto", options, index=0)
+if st.button("Informar falha"):
+    msg = grafo.remover_ares(int(vert1), int(vert2))
+    st.write(msg)
+st.subheader("Informar defeito em um transformador, gerador ou subestação")
+vert = st.selectbox("Ponto de falha", options, index=0)
+if st.button("Informar defeito"):
+    msg = grafo.remover_vert(int(vert))
     st.write(msg)
